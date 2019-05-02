ed53c150192d629ff5bd81873e38da6b68f78ddd
woody <vicbaily528@163.com>
2019-05-02 23:37:20 +0800

    添加LogBack日志信息

--- wintertea-service-user/src/main/java/com/wintertea/service/user/services/controller/UserController.java
+++ wintertea-service-user/src/main/java/com/wintertea/service/user/services/controller/UserController.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageInfo;
 import com.wintertea.service.user.commons.exception.WinterTeaException;
 import com.wintertea.service.user.services.service.IUserService;
 import com.wintertea.service.user.services.vo.UserResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     IUserService userService;
 
     @GetMapping("/{pageNo}/{pageSize}")
     public PageInfo<UserResp> selectByPage(@PathVariable("pageNo") int pageNo, @PathVariable("pageSize") int pageSize) throws InstantiationException, IllegalAccessException, WinterTeaException {
+        LOGGER.info("selectByPage {} {}", pageNo, pageSize);
         return userService.selectByPage(pageNo, pageSize);
     }
 }
--- wintertea-service-user/src/main/java/com/wintertea/service/user/services/service/impl/UserServiceImpl.java
+++ wintertea-service-user/src/main/java/com/wintertea/service/user/services/service/impl/UserServiceImpl.java
@@ -8,6 +8,8 @@ import com.wintertea.service.user.services.dao.UserEntityMapper;
 import com.wintertea.service.user.services.entity.UserEntity;
 import com.wintertea.service.user.services.service.IUserService;
 import com.wintertea.service.user.services.vo.UserResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +18,14 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements IUserService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     UserEntityMapper userEntityMapper;
 
     @Override
     public PageInfo<UserResp> selectByPage(int pageNo,int pageSize) throws IllegalAccessException, InstantiationException, WinterTeaException {
+        LOGGER.info(" UserServiceImpl ");
         PageHelper.startPage(pageNo,pageSize);
         List<UserEntity> userEntities = userEntityMapper.selectAll();
         return BeanHelper.toPageInfo(userEntities, UserResp.class);
